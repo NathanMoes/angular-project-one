1ee152244fa1977093e790c10330c4447887cdf5
Nathan Moes <nathanmoes7@gmail.com>
2023-04-30 10:17:15 -0700

    added recipe list to book

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
 
 @NgModule({
-  declarations: [AppComponent, ShoppingListComponent, RecipeBookComponent],
+  declarations: [
+    AppComponent,
+    ShoppingListComponent,
+    RecipeBookComponent,
+    HeaderComponent,
+    RecipeListComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
