57e003e3a870f454df4e0cb993009dd80560f30a
Nathan Moes <nathanmoes7@gmail.com>
2023-04-30 11:06:17 -0700

    configure of recipe display by item

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -13,5 +13,10 @@ export class RecipeListComponent {
       'a test',
       'https://cdn.tasteatlas.com/images/recipes/95d3d96a65444de183a6c64f8aeadd25.jpg'
     ),
+    new Recipe(
+      'A test',
+      'a test',
+      'https://cdn.tasteatlas.com/images/recipes/95d3d96a65444de183a6c64f8aeadd25.jpg'
+    ),
   ];
 }
