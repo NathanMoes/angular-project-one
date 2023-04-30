86877bb7a8ec3d13ca6052c8c3c189591e89c6e2
Nathan Moes <nathanmoes7@gmail.com>
2023-04-30 10:19:36 -0700

    shopping list edit added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeaderComponent } from './header/header.component';
 import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recip
     RecipeListComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
+    ShoppingEditComponent,
   ],
   imports: [BrowserModule],
   providers: [],
