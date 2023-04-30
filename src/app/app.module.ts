6020c152543649d026fc11cb48ea4c827407c2ea
Nathan Moes <nathanmoes7@gmail.com>
2023-04-30 10:01:14 -0700

    added header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, ShoppingListComponent, RecipeBookComponent],
+  declarations: [AppComponent, ShoppingListComponent, RecipeBookComponent, HeaderComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
