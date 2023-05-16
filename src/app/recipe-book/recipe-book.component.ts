c724ccdb7d5aea2695483751d0a09bc67bb8f947
Nathan Moes <nathanmoes7@gmail.com>
2023-05-16 13:29:42 -0700

    used subject for shopping list instead of emiter

--- src/app/recipe-book/recipe-book.component.ts
+++ src/app/recipe-book/recipe-book.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from './recipe.model';
 import { RecipeService } from './recipe.service';
 
 @Component({
@@ -8,13 +7,7 @@ import { RecipeService } from './recipe.service';
   providers: [RecipeService],
 })
 export class RecipeBookComponent implements OnInit {
-  activeRecipe: Recipe;
+  constructor() {}
 
-  constructor(private recipeService: RecipeService) {}
-
-  ngOnInit(): void {
-    this.recipeService.recipeSelected.subscribe((recipe: Recipe) => {
-      this.activeRecipe = recipe;
-    });
-  }
+  ngOnInit(): void {}
 }
