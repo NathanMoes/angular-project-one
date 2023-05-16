fa7332235c36f57d16005a2f78a0f7f18dc99f35
Nathan Moes <nathanmoes7@gmail.com>
2023-05-16 10:47:39 -0700

    updated routing navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,8 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
-  @Output() feature = new EventEmitter<string>();
-
-  onSelect(feature: string) {
-    this.feature.emit(feature);
-  }
-}
+export class HeaderComponent {}
