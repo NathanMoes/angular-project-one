cca5c96c6ebbab71697550da5d1f9e3c01aeb96f
Nathan Moes <nathanmoes7@gmail.com>
2023-05-19 18:55:42 -0700

    auth component updates

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
 })
-export class AuthComponent {}
+export class AuthComponent {
+  loginMode = true;
+
+  onSwitchMode() {
+    this.loginMode = !this.loginMode;
+  }
+
+  onSubmit(authForm: NgForm) {
+    authForm.reset();
+  }
+}
