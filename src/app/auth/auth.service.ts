f123d9bc0a784921953293d39829e9683a234cc8
Nathan Moes <nathanmoes7@gmail.com>
2023-05-20 13:37:19 -0700

    added autoLogin feature if stored valid time limited login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -88,6 +88,29 @@ export class AuthService {
     return throwError(errMsg);
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+  }
+
   private handelAuth(
     email: string,
     uId: string,
@@ -97,5 +120,6 @@ export class AuthService {
     const expDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, uId, token, expDate);
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 }
